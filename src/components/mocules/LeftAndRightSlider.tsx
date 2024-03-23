b46ad7ac967710243b4af451c864cfdb7ad43744
tjqehd23 <96939334+AhnDongSup@users.noreply.github.com>
2024-03-23 20:43:31 +0900

    슬라이드 아웃 메뉴 컴포넌트 추가

--- src/components/mocules/LeftAndRightSlider.tsx
+++ src/components/mocules/LeftAndRightSlider.tsx
@@ -11,10 +11,11 @@ interface LeftAndRightSliderProps {
 const LeftAndRightSlider = ({ children, className, moreAreaWidth, isSlideEnabled = true }: LeftAndRightSliderProps) => {
 
   const [startX, setStartX] = useState<number | null>(null);
+  const [startY, setStartY] = useState<number | null>(null);
   const [isSwiping, setIsSwiping] = useState<boolean>(true);
   const [isMoving, setIsMoving] = useState<boolean>(false);
   const [isOpenMore, setIsOpenMore] = useState<boolean>(false);
-  // const [isOnClick, setIsOnClick] = useState<boolean>(true);
+  const [lockScroll, setLockScroll] = useState<boolean>(false);
 
   const sliderRef = useRef<HTMLLIElement | null>(null);
 
@@ -23,40 +24,74 @@ const LeftAndRightSlider = ({ children, className, moreAreaWidth, isSlideEnabled
     setIsSwiping(false)
   }
 
-  const handleTouchStart = (e: React.TouchEvent) => {
+  const handleTouchStart = (e: React.TouchEvent | React.MouseEvent) => {
     if (isSlideEnabled) {
-      setStartX(e.touches[0].clientX);
-      // setIsOnClick(true)
+      if ("touches" in e) {
+        setStartX(e.touches[0].clientX);
+        setStartY(e.touches[0].clientY);
+      } else {
+        setStartX(e.clientX);
+        setStartY(e.clientY);
+      }
       setIsMoving(true)
     }
   };
 
-  const handleTouchMove = (e: React.TouchEvent) => {
-    // setIsOnClick(false)
-    if (startX === null) return;
-    const currentX = e.touches[0].clientX;
-    const subtract = Math.abs(startX - currentX);
-
+  const handleTouchMove = (e: React.TouchEvent | React.MouseEvent) => {
+    if (startX === null || startY === null) return;
+    let currentX: number
+    let currentY: number
+    if ("touches" in e) {
+      currentX = e.touches[0].clientX;
+      currentY = e.touches[0].clientY;
+    } else {
+      currentX = e.clientX;
+      currentY = e.clientY;
+    }
+    const subtractX = Math.abs(startX - currentX);
+    const subtractY = Math.abs(startY - currentY);
     const sliderRefCurrent = sliderRef.current;
-    if (sliderRefCurrent) {
-      const transformPercentage = (subtract / sliderRefCurrent.clientWidth) * 100;
 
-      if (subtract > 20) {
+
+    if (subtractX < 100 && subtractY > 100) {
+      setLockScroll(true)
+      sliderRefCurrent.style.transform = `translateX(0%)`;
+      return
+    }
+
+    if (sliderRefCurrent) {
+      const transformPercentage = (subtractX / sliderRefCurrent.clientWidth) * 100;
+
+      if (subtractX > 0) {
         handleScrollLock();
       }
 
-      if (startX < currentX && subtract > 20) {
+      if (startX < currentX && subtractX > 20) {
         sliderRefCurrent.style.transform = `translateX(${isOpenMore ? `calc(${transformPercentage}% - ${moreAreaWidth}px)` : `${transformPercentage}%`})`;
-      } else if (startX > currentX && subtract > 20) {
+      } else if (startX > currentX && subtractX > 20) {
         sliderRefCurrent.style.transform = `translateX(-${transformPercentage}%)`;
       }
     }
   }
 
-  const handleTouchEnd = (e: React.TouchEvent) => {
-    if (startX === null) return;
-    const endX = e.changedTouches[0].clientX;
+  const handleTouchEnd = (e: React.TouchEvent | React.MouseEvent) => {
+    if (startX === null || startY === null) return;
+    let endX: number
+    if ("changedTouches" in e) {
+      endX = e.changedTouches[0].clientX;
+    } else {
+      endX = e.clientX;
+    }
     const subtract = Math.abs(startX - endX);
+
+    if (subtract < 100 && lockScroll) {
+      setLockScroll(false)
+      setStartX(null);
+      setStartY(null)
+      setIsSwiping(true);
+      unlock()
+      return
+    }
     const sliderRefCurrent = sliderRef.current;
 
     if (sliderRefCurrent && startX < endX && subtract > 20) {
@@ -68,13 +103,9 @@ const LeftAndRightSlider = ({ children, className, moreAreaWidth, isSlideEnabled
       setIsMoving(false)
       setIsOpenMore(true)
       sliderRefCurrent.style.transform = `translateX(-${moreAreaWidth}px)`;
-    } else {
-      if (sliderRefCurrent) {
-        setIsMoving(false)
-        sliderRefCurrent.style.transform = `translateX(0%)`;
-      }
     }
     setStartX(null);
+    setStartY(null)
     setIsSwiping(true);
     unlock()
   };
@@ -83,13 +114,13 @@ const LeftAndRightSlider = ({ children, className, moreAreaWidth, isSlideEnabled
   return (
     <li
       className={`relative ${isMoving ? "" : "transition duration-300"} ${className}`}
-      style={{ width: `calc(100vw + ${moreAreaWidth}px)` }}
+      style={{ width: `calc(100% + ${moreAreaWidth}px)` }}
       onTouchStart={(e) => handleTouchStart(e)}
       onTouchMove={(e) => handleTouchMove(e)}
-      onTouchEnd={(e) => {
-        // isOnClick && handleFunc && handleFunc()
-        handleTouchEnd(e)
-      }}
+      onTouchEnd={(e) => handleTouchEnd(e)}
+      onMouseDown={(e) => handleTouchStart(e)}
+      onMouseMove={(e) => handleTouchMove(e)}
+      onMouseUp={(e) => handleTouchEnd(e)}
       ref={sliderRef}
     >
       {children}
