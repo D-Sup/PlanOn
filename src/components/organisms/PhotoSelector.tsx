51fad329c94efb2b640c7c8586bef01595012e3b
tjqehd23 <96939334+AhnDongSup@users.noreply.github.com>
2024-03-24 23:53:44 +0900

    feat: 공통 컴포넌트 제작 (#1)
    사진 선택 컴포넌트(게시글 작성용)

--- src/components/organisms/PhotoSelector.tsx
+++ src/components/organisms/PhotoSelector.tsx
@@ -1,18 +1,39 @@
-import { useRef, useState } from "react";
+import { useRef } from "react";
 
-import ImageFrame from "../atoms/ImageFrame";
-import CheckBox from "../atoms/CheckBox";
+import useModalStack from "@/hooks/useModalStack";
+import usePhotoUpload from "@/hooks/usePhotoUpload";
+import { SetterOrUpdater } from "recoil";
+
+import PhotoChecker from "./PhotoChecker";
 
 import IconCirclePlus from "../../assets/images/icon-circle-plus.svg?react";
-import IconRecycle from "../../assets/images/icon-recycle.svg?react";
 
-const PhotoSelector = () => {
+import { PostFormValueType } from "@/store";
 
-  const [postFormState, setPostFormState] = useState([]);
+interface PhotoSelectorProps {
+  postFormState: PostFormValueType,
+  setPostFormState: SetterOrUpdater<PostFormValueType> | React.Dispatch<React.SetStateAction<PostFormValueType>>,
+}
+const PhotoSelector = ({ postFormState, setPostFormState }: PhotoSelectorProps) => {
 
   const fileInputRef = useRef<HTMLInputElement>(null);
 
+  const { isPhotoExtensionValid } = usePhotoUpload();
+  const { openModal } = useModalStack();
+
   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+    if (files && files.length > 0) {
+      const { isValid, fileArray, previewArray } = isPhotoExtensionValid(files)
+      if (isValid) {
+        setPostFormState(Prev => ({ ...Prev, photos: { checked: [], file: fileArray, preview: previewArray } }));
+      } else {
+        openModal("Alert", "유효하지 않은 파일입니다.", ["확인"], [null])
+        if (fileInputRef.current) {
+          fileInputRef.current.value = "";
+        }
+      }
+    }
   }
 
   const handleUploadClick = () => {
@@ -21,64 +42,37 @@ const PhotoSelector = () => {
     }
   };
 
-  const handleCheckBox = (id: number) => {
-  };
-
 
   return (
-    <div className="relative flex flex-col bg-background">
-      {postFormState.photos.preview.length === 0 ?
-        (
-          <div className="relative inline-block w-full min-h-[300px] rounded-[10px] text-center border-2 border-white">
-            <div
-              className="flex-center gap-[5px] cursor-pointer w-full h-full absolute-center"
-              onClick={handleUploadClick}
-            >
-              <IconCirclePlus width={20} height={20} fill={"var(--white)"} />
-              <label htmlFor="fileInput" className="a11y-hidden">fileInput</label>
-              <input
-                ref={fileInputRef}
-                className="sr-only"
-                onChange={handleImageChange}
-                id="fileInput"
-                type="file"
-                accept="image/*"
-                multiple
-              />
-              <span className="text-lg text-white">사진추가</span>
+    <>
+      <div className="flex flex-col bg-background">
+        {postFormState.photos?.preview.length === 0 ?
+          (
+            <div className="relative inline-block w-full min-h-[290px] rounded-[10px] text-center border-2 border-white">
+              <div
+                className="flex-center gap-[5px] cursor-pointer w-full h-full absolute-center"
+                onClick={handleUploadClick}
+              >
+                <IconCirclePlus width={20} height={20} fill={"var(--white)"} />
+                <label htmlFor="fileInput" className="a11y-hidden">fileInput</label>
+                <input
+                  ref={fileInputRef}
+                  className="sr-only"
+                  onChange={handleImageChange}
+                  id="fileInput"
+                  type="file"
+                  accept="image/*"
+                  multiple
+                />
+                <span className="text-lg text-white">사진추가</span>
+              </div>
             </div>
-          </div>
-        ) : (
-          <div className="relative">
-            <ul className="m-auto grid grid-cols-3 gap-1">
-              {postFormState.photos.preview.map((image, index) => (
-                <li key={index} className="relative aspect-square bg-background">
-                  <ImageFrame
-                    src={image}
-                    alt={`album-${index}`}
-                    className="aspect-square object-cover"
-                  />
-                  <CheckBox
-                    id={index}
-                    checkedBox={postFormState.photos.checked}
-                    checked={postFormState.photos.checked.includes(index)}
-                    handleFunc={handleCheckBox}
-                  />
-                </li>
-              ))}
-            </ul>
-          </div>
-        )
-      }
-      {postFormState.photos.preview.length !== 0 &&
-        <button
-          className="fixed bottom-[30px] right-[40px] w-[40px] h-[40px] rounded-full"
-          style={{ backgroundColor: "rgba(255, 255, 255, 0.9)" }}
-          type="button">
-          <IconRecycle className="absolute-center" width={15} height={15} fill={"var(--black)"} />
-        </button>
-      }
-    </div>
+          ) : (
+            <PhotoChecker photoState={postFormState} setPhotoState={setPostFormState} />
+          )
+        }
+      </div>
+    </>
   );
 };
 
