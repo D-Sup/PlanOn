8a833bf18799bd9334a5f77934e44fd521a3156b
tjqehd23 <96939334+AhnDongSup@users.noreply.github.com>
2024-03-25 20:17:02 +0900

    style: 공통 컴포넌트 제작 (#1)
    체크박스

--- src/components/atoms/CheckBox.tsx
+++ src/components/atoms/CheckBox.tsx
@@ -7,28 +7,30 @@ interface CheckBoxProps {
 
 const CheckBox = ({ id, checked, checkedBox, handleFunc }: CheckBoxProps) => {
 
-
   return (
     <div className="absolute top-0 w-full h-full cursor-pointer"
       key={id}
       onClick={() => handleFunc(id)}
     >
-      <label htmlFor={String(id)} className="absolute top-1 right-1 block text-xs font-bold text-white user-select-none shadow-outline-white rounded-full">
-        <input
-          className="absolute opacity-100 cursor-pointer h-0 w-0"
-          id={String(id)}
-          type="checkbox"
-          onChange={() => handleFunc(id)}
-          checked={checked}
-        />
+      <label className="absolute top-1 right-1 block text-xs font-bold text-white user-select-none shadow-outline-white rounded-full">
         {checkedBox &&
           <div
-            className={`absolute right-0 h-[25px] w-[25px] rounded-full transition duration-100 border text-sm text-center ${checked ? "text-white" : ""}`}
-            style={{ backgroundColor: checked ? "var(--black)" : "rgba(255,255,255,0.5)" }}
+            className={`absolute right-0 h-[25px] w-[25px] rounded-full transition duration-100 border text-sm text-center ${checked ? "text-[#FFF]" : ""}`}
+            style={{ backgroundColor: checked ? "#000" : "rgba(255,255,255,0.5)" }}
+            onChange={(e) => {
+              e.stopPropagation()
+              handleFunc(id)
+            }}
           >
             <span className="absolute-center">{checked && checkedBox.indexOf(id) + 1}</span>
           </div>}
       </label >
+      <input
+        className="absolute opacity-100 cursor-pointer h-0 w-0"
+        id={String(id)}
+        type="checkbox"
+      // checked={checked}
+      />
     </div >
   );
 };
