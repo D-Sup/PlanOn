19318805ea6b357dd568e3a797065b1c3f19b12b
tjqehd23 <96939334+AhnDongSup@users.noreply.github.com>
2024-03-25 22:44:05 +0900

    feat: 공통 컴포넌트 제작 (#1)
    댓글/채팅 인풋박스

--- src/components/organisms/InteractiveInput.tsx
+++ src/components/organisms/InteractiveInput.tsx
@@ -1,64 +1,143 @@
-import React, { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect, useContext } from "react";
+import { UserContext } from "./UserInfoProvider";
+
+import usePhotoUpload from "@/hooks/usePhotoUpload";
+import useModalStack from "@/hooks/useModalStack";
 
 import ProfileAvatar from "../atoms/ProfileAvatar";
+import PhotoChecker from "./PhotoChecker";
 
 import IconSubmit from "../../assets/images/icon-submit.svg?react";
-import iconSendPhoto from "../../assets/images/icon-send-photo.svg";
+import IconSendPhoto from "../../assets/images/icon-send-photo.svg?react";
 
-const InteractiveInput = () => {
+import { PostFormValueType } from "@/store";
 
-  const [inptValue, setInptValue] = useState<string>("");
+interface InteractiveInputProps {
+  isCommentOnly?: boolean,
+  inputValue: string,
+  setInputValue: React.Dispatch<React.SetStateAction<string>>,
+  photoState?: PostFormValueType,
+  setPhotoState?: React.Dispatch<React.SetStateAction<PostFormValueType>>,
+  handleFunc: () => void,
+}
+
+const InteractiveInput = ({ isCommentOnly = true, inputValue, setInputValue, photoState, setPhotoState, handleFunc }: InteractiveInputProps) => {
+
+  const { data: userData, isLoading } = useContext(UserContext);
+
+  const [isOpenPhotoChecker, setIsOpenPhotoChecker] = useState<boolean>(false);
   const [prevInpt, setPrevInpt] = useState<string>("");
-  // const [commentSectionHeight, setCommentSectionHeight] = useState<number>(0);
   const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const virtualRef = useRef<HTMLDivElement>(null);
 
-  const isCommentPage = false;
-
+  useEffect(() => {
+    if (inputValue === "") {
+      setPrevInpt("")
+      textareaRef.current.style.height = "auto";
+    }
+  }, [inputValue])
 
   const handleTextareaChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setInptValue(e.target.value);
-    setPrevInpt(inptValue)
+    setInputValue(e.target.value);
+    setPrevInpt(inputValue)
   };
 
   useEffect(() => {
     const textareaRefCurrent = textareaRef.current
-    if (textareaRefCurrent) {
-      if (inptValue.length < prevInpt.length) {
+    const virtualRefCurrent = virtualRef.current
+    if (textareaRefCurrent && virtualRefCurrent) {
+      if (inputValue.length < prevInpt.length) {
         textareaRefCurrent.style.height = "auto";
       }
       const newHeight = textareaRefCurrent.scrollHeight;
-      // setCommentSectionHeight(newHeight)
       textareaRefCurrent.style.height = `${newHeight}px`;
       textareaRefCurrent.scrollTop = textareaRefCurrent.scrollHeight;
+      virtualRefCurrent.style.minHeight = `${textareaRefCurrent.scrollHeight + 32 <= 117 ? textareaRefCurrent.scrollHeight + 32 : 117}px`;
     }
-  }, [inptValue])
+  }, [inputValue])
+
+
+
+  const { isPhotoExtensionValid } = usePhotoUpload();
+  const { openModal } = useModalStack();
+
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+    if (files && files.length > 0) {
+      const { isValid, fileArray, previewArray } = isPhotoExtensionValid(files)
+      if (isValid && setPhotoState) {
+        setPhotoState(Prev => ({ ...Prev, photos: { checked: [], file: fileArray, preview: previewArray } }));
+        setIsOpenPhotoChecker(true)
+      } else {
+        openModal("Alert", "유효하지 않은 파일입니다.", ["확인"], [null])
+        if (fileInputRef.current) {
+          fileInputRef.current.value = "";
+        }
+      }
+    }
+  }
+
+  const handleUploadClick = () => {
+    if (fileInputRef.current && !isCommentOnly) {
+      fileInputRef.current.click();
+    }
+  };
 
 
   return (
     <>
-      <div className="px-[15px] py-4 flex items-end justify-between w-full bg-background">
-        <ProfileAvatar
-          className="w-[37px] h-[37px]"
+      <div
+        className={`fixed bottom-0 px-[15px] box-border py-4 flex items-end justify-between w-full bg-background transition duration-300`}
+        style={{ transform: isOpenPhotoChecker && photoState && photoState.photos.file.length !== 0 ? "translateY(-200px)" : "translateY(0)" }}
+      >
+        {!isLoading && <ProfileAvatar
+          handleFunc={handleUploadClick}
+          className="w-[44px] h-[44px]"
           alt="self-profile"
-          src={isCommentPage ? "https://cdn.pixabay.com/photo/2018/08/14/13/23/ocean-3605547_1280.jpg" : iconSendPhoto} />
-        <div className="relative min-h-10 w-[calc(100%-50px)] flex items-center">
+          src={isCommentOnly ? userData?.data?.accountImage as string : IconSendPhoto} />}
+        <div className="relative w-[calc(100%-60px)] flex items-center">
           <label htmlFor="interactive-input" className="a11y-hidden">interactive-input</label>
           <textarea
             rows={1}
             name="productInfo"
             id="interactive-input"
-            value={inptValue}
+            value={inputValue}
             onChange={handleTextareaChange}
             placeholder="입력"
-            className="py-[6px] pl-[15px] pr-[40px] w-full h-[37px] max-h-[80px] box-border rounded-[10px] resize-none bg-input text-sm text-base text-white overflow-y-scroll scroll-hide"
+            className="py-[10px] pl-[15px] pr-[50px] w-full max-h-[85px] rounded-[10px] resize-none bg-input text-md text-base text-white overflow-y-scroll scroll-hide"
             style={{ transition: "height .3s" }}
             ref={textareaRef}
           />
-          <button className="absolute bottom-[5px] right-[15px] transform -translate-y-1/2">
-            <IconSubmit width={15} height={15} fill={inptValue.length > 0 ? "var(--white)" : "var(--gray-heavy)"} />
+          <button
+            onClick={() => {
+              textareaRef.current.focus()
+              if ((inputValue.length !== 0 || photoState && photoState.photos.checked.length !== 0)) {
+                handleFunc()
+              }
+            }
+            }
+            className="p-[10px] absolute bottom-[-18px] right-[5px] transform -translate-y-1/2"
+          >
+            <IconSubmit width={20} height={20} fill={inputValue.length > 0 || (photoState && photoState.photos.checked.length > 0) ? "var(--white)" : "var(--gray-heavy)"} />
           </button>
         </div>
       </div>
+      <div className="fixed bottom-0 left-0 right-0 h-[200px] overflow-x-scroll transition duration-300 bg-background" style={{ transform: isOpenPhotoChecker && photoState?.photos.file.length !== 0 ? "translateY(0)" : "translateY(100%)" }}>
+        {isOpenPhotoChecker && photoState && setPhotoState && <PhotoChecker photoState={photoState} setPhotoState={setPhotoState} isGridAutoFlow={true} />}
+      </div>
+      <label htmlFor="fileInput" className="a11y-hidden">fileInput</label>
+      <input
+        ref={fileInputRef}
+        className="sr-only"
+        onChange={handleImageChange}
+        id="fileInput"
+        type="file"
+        accept="image/*"
+        multiple
+      />
+      <div className="w-full max-h-[85px]" ref={virtualRef}></div>
     </>
   )
 }
