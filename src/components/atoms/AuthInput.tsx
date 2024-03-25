b9d7e08c050ef859e6e90a84ba05a677e8c869db
tjqehd23 <96939334+AhnDongSup@users.noreply.github.com>
2024-03-25 20:17:02 +0900

    style: 공통 컴포넌트 제작 (#1)
    로그인/회원가입 인풋

--- src/components/atoms/AuthInput.tsx
+++ src/components/atoms/AuthInput.tsx
@@ -1,24 +1,25 @@
-import { useState } from "react"
-
+import { useState } from "react";
 import IconEye from "../../assets/images/icon-eye.svg?react";
 import IconInfo from "../../assets/images/icon-info.svg?react";
 
-const AuthInput = ({ id, type, placeholder, validation, invalidMessage }: { id: string, type: string, placeholder: string, validation: boolean, invalidMessage: string }) => {
+const AuthInput = ({ value, handleInputChange, id, type, placeholder, validation, invalidMessage }: { value: string, handleInputChange: (value: string) => void, id: string, type: string, placeholder: string, validation: boolean, invalidMessage: string }) => {
 
-  const [input, setInput] = useState<string>("");
+  const [isPasswordHide, setIsPasswordHide] = useState<boolean>(true);
 
   return (
-    <>
+    <div className="relative">
       <div className={`px-[15px] flex items-center w-full h-[50px] rounded-[10px] border transition duration-300 ${validation ? "border-gray-old" : "border-red"}`}>
         <label htmlFor={id} className="a11y-hidden">{id}</label>
-        <input className="w-full text-md text-white" type={type} id={id} placeholder={placeholder} value={input} onChange={((e) => setInput(e.target.value))} autoComplete="off" />
-        <IconEye className={`ml-auto ${type === "password" ? "block" : "hidden"}`} width={20} height={15} fill={"var(--input)"} />
+        <input className="w-full text-md text-white" type={type === "password" && isPasswordHide ? "password" : type !== "password" ? type : "text"} id={id} placeholder={placeholder} value={value} onChange={((e) => handleInputChange(e.target.value))} autoComplete="off" />
+        <button type="button" className="py-[10px]" onClick={() => setIsPasswordHide(!isPasswordHide)}>
+          <IconEye className={`ml-auto transition duration-300 ${type === "password" ? "block" : "hidden"}`} width={20} height={15} fill={isPasswordHide && value ? "var(--white)" : "var(--input)"} />
+        </button>
       </div>
-      <div className={`mt-[5px] flex items-center justify-end gap-[5px] transition duration-300 ${validation ? "hidden" : "block"}`}>
-        <IconInfo width={12} height={12} fill={"var(--red)"} />
+      <div className={`absolute bottom-[-20px] right-0 mt-[5px] flex items-center justify-end gap-[5px] transition duration-300 ${validation ? "opacity-0" : "opacity-100"}`}>
+        {invalidMessage && <IconInfo width={12} height={12} fill={"var(--red)"} />}
         <span className="text-xsm text-red">{invalidMessage}</span>
       </div>
-    </>
+    </div>
   )
 }
 
