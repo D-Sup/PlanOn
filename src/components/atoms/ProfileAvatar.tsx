8f05a379bd5c77a26a56f1ba0350bf300e448ba8
tjqehd23 <96939334+AhnDongSup@users.noreply.github.com>
2024-03-26 22:17:39 +0900

    style: 공통 컴포넌트 제작 (#1)
    프로필 아바타 컴포넌트 추가

---
+++ src/components/atoms/ProfileAvatar.tsx
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from "react";
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@radix-ui/react-avatar";
+import Loader from "../organisms/Loader";
+import { Skeleton } from "../shadcnUIKit/skeleton";
+
+import IconLogo from "../../assets/images/icon-logo.svg?react";
+
+const ProfileAvatar = ({ src, className, alt, handleFunc }: {
+  src: string | React.FunctionComponent<React.SVGProps<SVGSVGElement>>,
+  className: string,
+  alt: string,
+  handleFunc?: () => void
+}) => {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = typeof src === "string" && src;
+    img.onload = () => setIsLoaded(true);
+  }, [src]);
+
+  const widthAndHeightPattern = /w-\[\d+px\]|h-\[\d+px\]/g;
+  const widthAndHeight = className.match(widthAndHeightPattern);
+
+  return (
+    <div className={`${widthAndHeight !== null ? widthAndHeight.join(" ") : ""} relative rounded-full overflow-hidden bg-input`} onClick={handleFunc}>
+      {typeof src !== "string"
+        ? (
+          <div className="absolute-center">
+            {React.createElement(src, { width: "20", height: "20", fill: "var(--gray-old)" })}
+          </div>
+        ) : (
+          src !== ""
+            ? (
+              <>
+                {!isLoaded && <Skeleton className="min-w-full h-full rounded-full" />}
+                <img
+                  src={src}
+                  alt={alt}
+                  className={`${className} min-w-full h-full object-cover transition-opacity duration-300 ${isLoaded ? "opacity-100" : "opacity-0"}`}
+                />
+              </>
+            )
+            : <IconLogo fill={"var(--white)"} className="absolute-center w-1/3" />
+        )
+      }
+    </div>
+  )
+}
+
+export default ProfileAvatar;
+
+
+// <img src={src} className={`${className} rounded-full object-cover`} alt={alt} />
+// <Avatar>
+//   <AvatarImage src={src} className={`${className} rounded-full object-cover`} alt={alt} />
+//   <AvatarFallback>
+//     <div className="absolute-center w-[20px] h-[5px]">
+//       <Loader isSmallUse={true} />
+//     </div>
+//   </AvatarFallback>
+// </Avatar>
