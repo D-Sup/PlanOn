b1d0f757b7b280fe169c729be7377d0a0294cf35
tjqehd23 <96939334+AhnDongSup@users.noreply.github.com>
2024-04-14 21:58:27 +0900

    feat: 페이지 단위 컴포넌트 추가 - 스토리 디테일 페이지

---
+++ src/components/pages/PostDetailPage.tsx
@@ -0,0 +1,56 @@
+import { useEffect } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import { useRecoilState } from "recoil"
+import { routeDirectionValue } from "@/store"
+
+import PostService from "@/services/postService";
+
+import getAccountId from "@/utils/getAccountId";
+
+import FixedTrigger from "../mocules/FixedTrigger";
+import Header from "../organisms/Header";
+import PostCard from "../organisms/PostCard";
+import PostCardSkeleton from "../skeleton/PostCardSkeleton";
+
+const PostDetailPage = () => {
+
+  const navigate = useNavigate()
+  const location = useLocation()
+  const [routeDirectionValueState, setRouteDirectionValueState] = useRecoilState(routeDirectionValue)
+
+  const { post, isReadOnly } = location.state || []
+
+  const accountId = getAccountId()
+
+  const { ReadPostSingle } = PostService()
+  const { data, isLoading, refetch } = ReadPostSingle(post)
+
+  useEffect(() => {
+    refetch()
+  }, [])
+
+  const { DetailHeader, DownloadSuggestHeader } = Header()
+
+  const goBack = () => {
+    if (routeDirectionValueState.previousPageUrl.length === 0) {
+      navigate("/post", { state: { direction: "prev" } })
+    } else {
+      setRouteDirectionValueState(Prev => ({ ...Prev, direction: "prev" }))
+    }
+  }
+
+  return (
+    <>
+      {isReadOnly && !accountId &&
+        <DownloadSuggestHeader />
+      }
+      <FixedTrigger className="top-0" height={40} enableAnimation={false}>
+        {!isReadOnly && <DetailHeader title={"게시물 상세"} handleFunc={goBack} />}
+      </FixedTrigger>
+      {isLoading && <PostCardSkeleton repeat={1} />}
+      {data && <PostCard data={data} isReadOnly={isReadOnly} />}
+    </>
+  )
+}
+
+export default PostDetailPage
