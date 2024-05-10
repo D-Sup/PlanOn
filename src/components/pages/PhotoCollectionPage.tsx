30708737236b440bad8facc469b8f356004c8e3b
tjqehd23 <96939334+AhnDongSup@users.noreply.github.com>
2024-05-10 21:20:39 +0900

    feat: 페이지 단위 컴포넌트 생성 - 사진 모아보기 페이지

---
+++ src/components/pages/PhotoCollectionPage.tsx
@@ -0,0 +1,46 @@
+import { useLocation } from "react-router-dom"
+import useModalStack from "@/hooks/useModalStack"
+import { useSetRecoilState } from "recoil"
+import { routeDirectionValue } from "@/store"
+
+import Header from "../organisms/Header"
+import FixedTrigger from "../mocules/FixedTrigger"
+import ImageFrame from "../atoms/ImageFrame"
+
+const PhotoCollectionPage = () => {
+
+  const setRouteDirectionValueState = useSetRecoilState(routeDirectionValue);
+
+  const { openModal } = useModalStack()
+
+  const { DetailHeader } = Header()
+
+  const location = useLocation()
+
+  const { photos } = location.state || {};
+
+  return (
+    <>
+      <FixedTrigger className="top-0 w-full" height={40} enableAnimation={false}>
+        <DetailHeader title={"사진"} handleFunc={() =>
+          setRouteDirectionValueState(Prev => ({ ...Prev, direction: "prev" }))} />
+      </FixedTrigger>
+
+      <ul className="m-auto grid grid-cols-2 gap-1">
+        {photos?.map((photo: string) => (
+          <li
+            key={photo}
+            className="relative aspect-square bg-background"
+            onClick={() => {
+              openModal("PhotoView", { photo })
+            }}
+          >
+            <ImageFrame src={photo} alt={`photo-${photo}`} />
+          </li>
+        ))}
+      </ul>
+    </>
+  )
+}
+
+export default PhotoCollectionPage
