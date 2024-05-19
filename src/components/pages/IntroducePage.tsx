2b0d574d57d4926a86e8edf78ba271dbb2521ef1
tjqehd23 <96939334+AhnDongSup@users.noreply.github.com>
2024-05-19 23:39:12 +0900

    feat: 페이지 단위 컴포넌트 생성 - 앱소개 페이지

---
+++ src/components/pages/IntroducePage.tsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import IconLogo from "../../assets/images/icon-logo.svg?react";
+
+const IntroducePage = () => {
+
+  const navigate = useNavigate()
+
+  const [isWait, setIsWait] = useState<boolean>(true);
+
+  useEffect(() => {
+    setTimeout(() => setIsWait(false), 1000)
+  }, [])
+
+  return (
+    <>
+      {
+        isWait ? (
+          <></>
+        ) : (
+          <div className="relative flex-center flex-col h-dvh">
+            <IconLogo fill={"var(--white)"} className="mt-[-100px] w-1/4 h-1/4 opacity-0 animate-show-content-1" />
+
+            <h2 className="text-white text-2xl opacity-0 animate-show-content-2">플랜온</h2>
+            <p className="mt-[10px] text-white text-md opacity-0 animate-show-content-3">당신의 하루를 계획하고, 공유하세요</p>
+
+            <div className="absolute bottom-[30px] px-[30px] w-full opacity-0 animate-show-content-4">
+              <button
+                type="button"
+                className="w-full h-[50px] rounded-[10px] bg-white text-lg text-black font-bold"
+                onClick={() => navigate("/login", { state: { direction: "next" } })}
+
+              >
+                시작하기
+              </button>
+              <span className="mt-[10px] block text-gray-old text-center text-sm">회원가입 후 언제든지 간편하게 탈퇴하실 수 있습니다. 😊</span>
+            </div>
+          </div>
+
+        )
+      }
+    </>
+
+  )
+}
+
+export default IntroducePage
