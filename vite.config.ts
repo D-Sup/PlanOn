62b3700349c134b27dd5751fd53acf09457ac9b0
tjqehd23 <96939334+AhnDongSup@users.noreply.github.com>
2024-05-30 21:18:18 +0900

    feat: Vite 설정 파일 추가 및 PWA 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,60 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import svgrPlugin from "vite-plugin-svgr";
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig, loadEnv } from "vite"
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [
+      react(), svgrPlugin(),
+        VitePWA({ 
+          registerType: "autoUpdate",
+          devOptions: {
+            enabled: true,
+          },
+          includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+          manifest: {
+            name: "플랜온",
+            short_name: "플랜온",
+            display: "standalone",
+            theme_color: "#1A1A1A",
+            icons: [
+                {
+                    src: "pwa-64x64.png",
+                    sizes: "64x64",
+                    type: "image/png"
+                },
+                {
+                    src: "pwa-192x192.png",
+                    sizes: "192x192",
+                    type: "image/png"
+                },
+                {
+                    src: "pwa-512x512.png",
+                    sizes: "512x512",
+                    type: "image/png",
+                    purpose: "any"
+                },
+                {
+                    src: "maskable-icon-512x512.png",
+                    sizes: "512x512",
+                    type: "image/png",
+                    purpose: "maskable"
+                }
+            ],
+          }, 
+        })
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-})
+    define: {
+      "process.env": JSON.stringify(env),
+    }
+  };
+});
