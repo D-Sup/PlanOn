9b17d8a70fe1953466ad3667c5fb18bbbcd43a3a
tjqehd23 <96939334+AhnDongSup@users.noreply.github.com>
2024-06-04 02:31:37 +0900

    added

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,52 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import svgrPlugin from "vite-plugin-svgr";
+import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig, loadEnv } from "vite"
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
   return {
-    plugins: [react(), svgrPlugin()],
+    plugins: [
+      react(), svgrPlugin(),
+        VitePWA({ 
+          registerType: "autoUpdate",
+          devOptions: {
+            enabled: true,
+          },
+          includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+          manifest: {
+            name: "Vite PWA Project",
+            short_name: "Vite PWA Project",
+            theme_color: "#ffffff",
+            icons: [
+                {
+                    src: "pwa-64x64.png",
+                    sizes: "64x64",
+                    type: "image/png"
+                },
+                {
+                    src: "pwa-192x192.png",
+                    sizes: "192x192",
+                    type: "image/png"
+                },
+                {
+                    src: "pwa-512x512.png",
+                    sizes: "512x512",
+                    type: "image/png",
+                    purpose: "any"
+                },
+                {
+                    src: "maskable-icon-512x512.png",
+                    sizes: "512x512",
+                    type: "image/png",
+                    purpose: "maskable"
+                }
+            ],
+          }, 
+        })
+    ],
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
